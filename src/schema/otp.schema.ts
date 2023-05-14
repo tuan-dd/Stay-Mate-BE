0ae21d286a6ad54239cf7c5226099ae97b94b67c
tuan-dd <tuandd.310797@gmail.com>
2023-05-14 16:11:26 +0700

    #22 fix bug

--- src/schema/otp.schema.ts
+++ src/schema/otp.schema.ts
@@ -3,7 +3,7 @@ import * as Yup from 'yup';
 export const otpSchema = Yup.object().shape({
   body: Yup.object().shape({
     email: Yup.string().email().required(),
-    sixCode: Yup.number().min(100_000).max(999_999),
+    sixCode: Yup.number().min(100_000).max(999_999).required(),
   }),
 });
 
