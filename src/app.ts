698c64394627938fa80f13e448679a60551b62d3
tuan-dd <tuandd.310797@gmail.com>
2023-05-25 03:19:08 +0700

    #22 add url

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ class App {
   private allowlist = [
     'http://localhost:5000',
     'http://localhost:3000',
-    'https://staymate-tuan.netlify.app/',
+    'https://staymate-tuan.netlify.app',
   ];
 
   constructor() {
