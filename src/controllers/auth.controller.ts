60dd5efa5a34183ef76d1708d84f80dd697c2043
tuan-dd <tuandd.310797@gmail.com>
2023-06-10 18:06:34 +0700

    #28 test

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -136,7 +136,6 @@ class AuthController {
         httpOnly: false,
         secure: true,
         path: '/',
-        sameSite: 'none',
         domain: 'profound-duckanoo-5b6f6f.netlify.app',
         maxAge: 86400000 * 7,
       })
@@ -144,7 +143,6 @@ class AuthController {
         httpOnly: false,
         secure: true,
         path: '/',
-        sameSite: 'none',
         domain: 'profound-duckanoo-5b6f6f.netlify.app',
         maxAge: 86400000 * 7,
       });
